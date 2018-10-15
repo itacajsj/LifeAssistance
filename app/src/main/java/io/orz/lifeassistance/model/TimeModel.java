ff7e8950f9bae722bffcb067865bd1c3972d3bf7
itacajsj <itacajsj@outlook.com>
2018-10-16 00:04:07 +0800

    feat(Clock):时钟view
    时钟控件(30%)

---
+++ app/src/main/java/io/orz/lifeassistance/model/TimeModel.java
@@ -0,0 +1,16 @@
+package io.orz.lifeassistance.model;
+
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+/**
+ * Created by itaca on 2018/10/15.
+ */
+
+public class TimeModel {
+    private Date now;
+
+    private Date getNow(){
+        return now;
+    }
+}
